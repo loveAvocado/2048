8a78bb91421449460daf312f04b557dd77dba66e
loveavocado <jeun.chien.t@Outlook.com>
2023-01-24 01:29:45 +0800

    Create first2048.cpp

---
+++ first2048.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+#include <cstring>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+// This is an attempt to build a very simple and crude version of 2048 game
+// with no beautiful UI but only ASCII style one.
+
+int main () {
+    int** square[4][4]; 
+    int score = 0; //　記錄得分
+    memset(square,0,sizeof(square));
+
+}
+
+// For each direction shifting, create a function to solve it.
+void LeftShift(){}
+
+void RightShift(){}
+
+void DownShift(){}
+
+void UpShift(){}
+
